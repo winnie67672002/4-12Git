515472e36f9ca49ee0557abe3fb57e286cd58b99
Wang <winnnie67672002@yahoo.com.tw>
2022-04-16 14:25:59 +0800

    在 local master 修改 2

--- 4-12Git/Program.cs
+++ 4-12Git/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("在 local master 修改 1");
+Console.WriteLine("在 local master 修改 2");
