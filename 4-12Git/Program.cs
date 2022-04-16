218931a2d7aca871b79bdd2a799910639bce24ba
Wang <winnnie67672002@yahoo.com.tw>
2022-04-16 14:30:20 +0800

    在 local master 修改 5

--- 4-12Git/Program.cs
+++ 4-12Git/Program.cs
@@ -5,3 +5,5 @@ Console.WriteLine("在 local master 「不」pull就修改3");
 Console.WriteLine("在 local master 「不」pull就修改4");
 Console.WriteLine("在 GitHub 修改模擬別人 Push 1");
 Console.WriteLine("在 GitHub 修改模擬別人 Push 2");
+
+Console.WriteLine("在 local master 修改 5");
